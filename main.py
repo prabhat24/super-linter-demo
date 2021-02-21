40a4f5b940e91b2fbaa8f764a614ebc255a40747
anzo <prabhat24katiyar@gmail.com>
2021-02-22 00:58:12 +0530

    commit main + sample

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
-from sample_file import feasible, find_max_distance
+from sample_file import find_max_distance
 
-if __name__=='__main__':
-  test_cases=int(input())
-  for i in range(test_cases):
-    ar = list()
-    str_n_cows = input()
-    N=int(str_n_cows.split(" ")[0])
-    cows = int(str_n_cows.split(" ")[1])
-    for j in range(N):
-      x = int(input())
-      ar.append(x)
-    ar.sort()
-    print(find_max_distance(ar, N, cows))
+if __name__ == "__main__":
+    test_cases = int(input())
+    not_used = "delete me"
+    for i in range(test_cases):
+        ar = list()
+        str_n_cows = input()
+        N = int(str_n_cows.split(" ")[0])
+        cows = int(str_n_cows.split(" ")[1])
+        for j in range(N):
+            x = int(input())
+            ar.append(x)
+        ar.sort()
+        print(find_max_distance(ar, N, cows))
--- sample_file.py
+++ sample_file.py
@@ -1,6 +1,8 @@
 # algo
 # use binary search to find the max feasible distance. use the search space   1.....ar[-1]-ar[0]
 #
+
+
 def feasible(ar, N, dis, no_of_cows):
     j = 1
     count_cows = 1
@@ -10,7 +12,7 @@ def feasible(ar, N, dis, no_of_cows):
             last_cow_loc = j
             count_cows += 1
         j += 1
-    if count_cows >=no_of_cows:
+    if count_cows >= no_of_cows:
         return True
     else:
         return False
@@ -18,16 +20,14 @@ def feasible(ar, N, dis, no_of_cows):
 
 def find_max_distance(ar, N, no_of_cows):
     st = 1
-    end = ar[-1] -ar[0]
+    end = ar[-1] - ar[0]
     ans = 1
-    while  st <= end :
-        mid = st + (end - st)// 2
-        if feasible(ar, N, mid,no_of_cows):
-            if mid>ans:
+    while st <= end:
+        mid = st + (end - st) // 2
+        if feasible(ar, N, mid, no_of_cows):
+            if mid > ans:
                 ans = mid
-            st = mid+1
+            st = mid + 1
         else:
-            end = mid -1
+            end = mid - 1
     return ans
-
-
